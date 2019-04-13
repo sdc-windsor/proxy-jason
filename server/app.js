c21092fc10894f5b1506092562cf8e4f90589d10
Jason Bourget <jason.andre.bourget@gmail.com>
2019-04-13 12:52:00 -0600

    Minor mods

--- server/app.js
+++ server/app.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const app = express();
 
-app.use('/', function (req, res, next) {
-  console.log('Time:', Date.now())
-  console.log('url', req.url);
-  next()
-});
-
 app.use(express.static('public'));
 app.use('/:id', express.static('public'));
 
