d1bc437b07bd29da956037b19a0b0c699498c495
Jason Bourget <jason.andre.bourget@gmail.com>
2019-04-13 12:39:29 -0600

    Made video recommendations tests dynamic

--- load-funcs.js
+++ load-funcs.js
@@ -1,17 +1,3 @@
-const generateVidRecBody = (id) => {
-  return JSON.stringify({
-    query: `{
-      getRecommendations(videoId:${id}) {
-        id,
-        author,
-        title,
-        thumbnail,
-        plays
-      }
-    }`,
-  });
-}
-
 const generateRandomVideo = (min = 9000000, max = 10000000) => {
   return min + Math.floor(Math.random() * (max - min));
 }
@@ -19,7 +5,5 @@ const generateRandomVideo = (min = 9000000, max = 10000000) => {
 exports.generateTestVars = (context, events, done) => {
   const id = generateRandomVideo();
   context.vars.id = id;
-  context.vars.videoRecBody = generateVidRecBody(id);
-  console.log({ id: context.vars.id, body: context.vars.videoRecBody})
   return done();
 }
